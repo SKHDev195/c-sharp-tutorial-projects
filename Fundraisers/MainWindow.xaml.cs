334b2808449ceb5d72a02aff569ae83bc14b3211
Shukhrat Khannanov <khannanov.sh@gmail.com>
2022-11-14 21:29:22 +0300

    First session with Fundraisers

---
+++ Fundraisers/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Fundraisers
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Class1 fundTest = new Class1();
+            this.FOneFunds.Text = fundTest.Funds;
+            this.ProgressFOne.Value = (double)fundTest.Funds;
+            this.Height = SystemParameters.PrimaryScreenHeight * 0.5;
+            this.Width = SystemParameters.PrimaryScreenWidth * 0.5;
+        }
+    }
+}
---
+++ TestWPFApp/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace TestWPFApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            NewResourcesExample wnd = new NewResourcesExample();
+            wnd.Title = "Test Title";
+            wnd.Show();
+        }
+
+        private void App_OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show("An unhandled exception just occurred: " + e.Exception.Message, "Exception Sample", MessageBoxButton.OK, MessageBoxImage.Error);
+            e.Handled = true;
+        }
+    }
+}
